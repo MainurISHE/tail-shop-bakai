b256395b59d1b1a957cfc4bc8cdb31c996fb5f4e
Mainur <kentykimainur@gmail.com>
2026-06-27 12:55:46 +0600

    feat: added adaptability, added FooterComponent

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Container } from '../../shared/ui/container/container';
 import { Header } from '../../shared/ui/header/header';
+import { FooterComponent } from '../../shared/ui/footer/footer';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [Container, RouterOutlet, Header],
+  imports: [Container, RouterOutlet, Header, FooterComponent],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
